faef60d68f57fcbfbf57cb97607f4564935800a9
bmongar <bmongar@keyholesoftware.com>
2021-10-26 17:29:53 -0500

    sparkmax

--- Practice Bot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Practice Bot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,17 +1,22 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.DrivetrainConstants;
 
 public class Drivetrain extends SubsystemBase{
-    private Spark frontLeftMotor = new Spark(Constants.DrivetrainConstants.FRONT_LEFT_MOTOR_ID);
-    private Spark frontRightMotor = new Spark(Constants.DrivetrainConstants.FRONT_RIGHT_MOTOR_ID);
-    private Spark backLeftMotor = new Spark(Constants.DrivetrainConstants.BACK_LEFT_MOTOR_ID);
-    private Spark backRightMotor = new Spark(Constants.DrivetrainConstants.BACK_RIGHT_MOTOR_ID);
+   
+    private CANSparkMax frontLeftMotor = new CANSparkMax(DrivetrainConstants.FRONT_LEFT_MOTOR_ID, MotorType.kBrushless);
+    private CANSparkMax frontRightMotor = new CANSparkMax(DrivetrainConstants.FRONT_RIGHT_MOTOR_ID, MotorType.kBrushless);
+    private CANSparkMax backLeftMotor = new CANSparkMax(DrivetrainConstants.BACK_LEFT_MOTOR_ID, MotorType.kBrushless);
+    private CANSparkMax backRightMotor = new CANSparkMax(DrivetrainConstants.BACK_RIGHT_MOTOR_ID, MotorType.kBrushless);
     private SpeedControllerGroup leftGroup = new SpeedControllerGroup(new SpeedController[] {frontLeftMotor,backLeftMotor});
     private SpeedControllerGroup rightGroup = new SpeedControllerGroup(new SpeedController[] {frontRightMotor,backRightMotor});
 
